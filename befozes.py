cf52b8fec3e86139d9ebda2893b456c930669def
durgo balazs <durgobali06@gmail.com>
2026-03-13 12:27:27 +0100

    befozes

---
+++ befozes.py
@@ -0,0 +1,44 @@
+uvegek = [5,2,2,4,3,2,4,10,5,5,3,5,4,3,3]
+
+def lekvar_mennyiseg():
+    dl = int(input(f"Mari néni lekvárja (dl):"))
+    
+    while dl <=0  or  dl > 200:
+        dl = int(input(f"Mari néni lekvárja (dl):"))
+    return dl
+
+def legnagyobb_uveg():
+    
+    maxi = 0
+    max_ertek = uvegek[0]
+    
+    for i in range(1, len(uvegek)):
+        if uvegek[i]> max_ertek:
+            max_ertek=uvegek[i]
+            maxi=i
+    return maxi
+
+def uveg_kapacitas():
+    osszeg = 0
+    
+    for uveg in uvegek:
+        osszeg +=uveg 
+    return osszeg
+# PROGRAM
+print(f"2.feladat")
+mennyiseg = lekvar_mennyiseg()
+print(f"3.feladat")
+legnagyobb= legnagyobb_uveg()
+
+print(f"A legnagyobb üveg: {uvegek[legnagyobb]}dl és {legnagyobb + 1 } sorban")
+print(f"4.feladat")
+meret = uveg_kapacitas()
+if meret > mennyiseg:
+    print(f"Elegendö üveg volt.")
+    i=0    
+    while mennyiseg > 0:
+        mennyiseg -= uvegek[i]
+    print(f"Megmaradt üvegek száma: {len(uvegek) - (i+1)}")
+else:
+    print(f"Maradt lekvár.")
+
